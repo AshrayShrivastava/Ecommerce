9e8f520f1a5d8c1548a0f13a155dbbf148f1e93d
AshrayShrivastava <ash.shrivastava.10@gmail.com>
2023-02-24 23:32:54 +0530

    basic frontend layout

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <div className="App">
+      
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
